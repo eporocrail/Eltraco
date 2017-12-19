52389c7f602fec0cc683a3d9bab748439e7e65a4
eporocrail <ellardpostma1@outlook.com>
2017-12-19 08:43:23 +0100

    MQTT broker details retrieved from Rocrail or default data used
    Liviu developed a software module to retrieve MQTT broker data from
    Rocrail. This module is slightly adapted and incorporated.

--- Eltraco_Receiver_v1.0/Eltraco_Receiver_v1.0.ino
+++ Eltraco_Receiver_v1.0/Eltraco_Receiver_v1.0.ino
@@ -4,6 +4,10 @@
   changelog:
 
   dec 2017:
+  software module added to retrieve MQTT broker IP-address and portnumber from Rocrail server.
+  software developed by Liviu, slightly adapted
+
+  dec 2017:
   new MQTT library. supports qos2. link for library: https://github.com/Imroy/pubsubclient
   Length of MQTT outgoing message equal to Rocnet message length.
   "yield()" in loop.
@@ -19,17 +23,25 @@
 #include <PubSubClient.h>
 #include <stdlib.h>
 #include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
 #include <SoftwareSerial.h>
 extern "C" {
 #include "user_interface.h"
 }
 
 WiFiClient espClient;
+WiFiUDP espUdpClient;
+IPAddress broadcastAddr(224, 0, 0, 1);
+uint16_t udpPort = 8051;
+
 
 #define WIFI_TX_POWER 0 // TX power of ESP module (0 -> 0.25dBm) (0...85)
 
 static char decoderId = 149;                                      // also used in IP address decoder (check if IP address is available)
 static char wiFiHostname[] = "ELTRACO-Receiver";                  // Hostname displayed in OTA port
+static const String topicSub = "rocnet/#";
+//static const String topicSub = "/#";                              // select topic from which message are received
+
 ///////////////////////
 /*
    Define which Wifi Network is to be used
@@ -42,25 +54,40 @@ static char wiFiHostname[] = "ELTRACO-Receiver";                  // Hostname di
 
 //////////////////////////// wifi network selection //////////////
 #if WIFI_NETWORK == 0
-static const char *ssid = "SSID1";                                        // ssid WiFi network
-static const char *password = "paswrd1";                                // password WiFi network
+static const char *ssid = "SSID1";                                         // ssid WiFi network
+static const char *password = "PASWRD1";                                   // password WiFi network
 #endif
 
 #if WIFI_NETWORK == 1
-static const char *ssid = "SSID2";                                            // ssid WiFi network
-static const char *password = "paswrd2";                                   // password WiFi network
+static const char *ssid = "SSID2";                                         // ssid WiFi network
+static const char *password = "PASWRD12";                                  // password WiFi network
 #endif
+///////////////////////////////////////////////////////////////////////////////////////////
+
 
 static const char *MQTTclientId = (wiFiHostname);                           // MQTT client Id
-IPAddress mosquitto(192, 168, 2, 1);                                        // IP address Mosquitto
+IPAddress mosquitto(192, 168, 2, 254);                                      // default IP address Mosquitto to be replaced with retrieved version
+uint16_t mqttPort = 1883;                                                   // default mosquitto port to be replaced with retrieved version
 IPAddress decoder(192, 168, 2, decoderId);                                  // IP address decoder
 IPAddress gateway(192, 168, 2, 1);                                          // IP address gateway
 IPAddress subnet(255, 255, 255, 0);                                         // subnet masker
 PubSubClient client(espClient, mosquitto);
+void callback(const MQTT::Publish& pub);
+/////////////////////////////////////////// IP-address retrieval /////////////////////////
+typedef struct {
+  IPAddress ip;
+  uint16_t port;
+} mqttBrokerDet_t;
+
+mqttBrokerDet_t myMqttBrokerDet;
+mqttBrokerDet_t detectIpPort(void);
+static byte retrievalCounter = 0;
+static byte retrievalNumber = 5;                              // number of retrieval attempts
+static boolean brokerDefault = false;
 ///////////////////////////////////////////////////////////////////////////////////////////
-//static const String topicSub = "rocnet/#";
-static const String topicSub = "/#";
-////////////////////////////// RFID reader /////////////////////////////
+
+
+
 ///////////////////////////////////////////////////////////////set-up//////////////////////////////
 void setup() {
   Serial.begin(9600);                                        // the Serial port of Wemos baud rate.
@@ -72,6 +99,20 @@ void setup() {
   setup_wifi();
   client.set_callback(callback);
 
+  //start the UDP client, to retrieve the broker address from Rocrail
+  espUdpClient.begin(udpPort);
+
+  //broker address request.
+  myMqttBrokerDet = detectIpPort();
+  if (brokerDefault == false) {
+    Serial.print(F("using retrieved data"));
+    client.set_server(myMqttBrokerDet.ip, myMqttBrokerDet.port);
+  } else {
+    Serial.print(F("using default data"));
+    client.set_server(mosquitto, mqttPort);
+  }
+  Serial.println();
+  Serial.println();
 }
 ///////////////////////////////////////////////////////////////end of set-up////////////////////////
 /////////////////////////////////////////////////////////////// program loop ////////////////////////////////
@@ -86,8 +127,7 @@ void loop() {
 /*
    callback
 
-   function : receive incoming message, test topic, test on recepient, select returning sensor messages, select servo configuration,
-   select incoming turnout switch orders, select turnout number, store order into buffer.
+   function : receive incoming message
 
 */
 void callback(const MQTT::Publish& pub) {
@@ -104,7 +144,7 @@ void callback(const MQTT::Publish& pub) {
 
 /*
    re-establish connection with MQTT clientID.
-   publish topic topicPub. subscribe to topic topicSub.
+   subscribe to topic topicSub.
    when Mosquitto not available try again after 5 seconds
 
 */
@@ -141,12 +181,96 @@ void setup_wifi() {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("");
-  Serial.println(F("WiFi connected"));
-  Serial.print(F("IP address: "));
+
+  Serial.print(F("\nWiFi connected using:"));
+  Serial.print(F("\nIP-address: "));
   Serial.println(WiFi.localIP());
-  Serial.print(F("hostname: "));
+  Serial.print(F("hostname:   "));
   Serial.println(WiFi.hostname());
 }
 // end of setup_wifi
 
+//-------------------------------------------------------------
+
+mqttBrokerDet_t detectIpPort(void) {
+  mqttBrokerDet_t mqttBr;
+  bool bAddressFound = false;
+  char packetBuffer[80];
+  uint16_t brokerPort = 0;
+  char ipAsCharBuf[25];
+  bool ipOk = false;
+  bool portOk = false;
+
+  if (WiFi.status() == WL_CONNECTED) {
+    if (!espUdpClient) {
+      espUdpClient.begin(udpPort);
+    }
+  }
+
+  do {
+    espUdpClient.beginPacketMulticast(broadcastAddr, udpPort, WiFi.localIP());
+    espUdpClient.write("BROKER-GET");
+    espUdpClient.endPacket();
+
+    int packetSize = espUdpClient.parsePacket();
+
+    if (packetSize) {
+      int len = espUdpClient.read(packetBuffer, 80);
+      if (len > 0) {
+        packetBuffer[len] = 0;
+      }
+      String udpMess = String(packetBuffer);
+      uint8_t brokerIpStart = udpMess.indexOf("BROKER-HOST");
+      uint8_t brokerPortStart = udpMess.indexOf("BROKER-PORT");
+
+      ipOk = false;
+      if (brokerIpStart != -1) {
+        String fullBrokerIp = udpMess.substring(11, brokerPortStart - 1);
+        String ip = fullBrokerIp.substring(fullBrokerIp.indexOf(":") + 1, brokerPortStart);
+        ip.toCharArray(ipAsCharBuf, ip.length() + 1);
+        ipOk = true;
+      }
+
+      portOk = false;
+      if (brokerPortStart != -1) {
+        String brokerPortMessage = udpMess.substring(brokerPortStart);
+        String brokerPortAsString = brokerPortMessage.substring(brokerPortMessage.lastIndexOf(":") + 1);
+        brokerPort = brokerPortAsString.toInt();
+        portOk = true;
+      }
+
+      if (ipOk && portOk) {
+        bAddressFound = true;
+        mqttBr.ip.fromString(ipAsCharBuf);
+        mqttBr.port = brokerPort;
+        Serial.print(F("\nMQTT broker details retrieved from Rocrail:"));
+        Serial.print(F("\nIP-address: "));
+        mqttBr.ip.printTo(Serial);
+        Serial.print(F("\nPORT:       "));
+        Serial.println(brokerPort);
+        Serial.println();
+      } //if(ipOk && portOk)
+    }
+
+    if (!bAddressFound) {
+      delay(1000);
+      if (retrievalCounter == 0) {
+        Serial.println(); 
+        Serial.println("retrieving MQTT broker data from Rocrail");
+      }
+      retrievalCounter++;
+      Serial.print("retrieval attempt: ");
+      Serial.println(retrievalCounter);
+      if (retrievalCounter > retrievalNumber) {
+        brokerDefault = true;
+        retrievalCounter = 0;
+        Serial.print(F("\nNo data from Rocrail retrieved"));
+        Serial.println();
+        Serial.println();
+        break;
+      }
+    }
+  } while (!bAddressFound);
+
+  return (mqttBr);
+} // end of mqttBrokerDet_t detectIpPort(void)
